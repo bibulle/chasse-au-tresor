f521139134cb0fae85dcfbe284aa53eb64228608
bibulle <famille.martin@gmail.com>
2024-11-23 09:26:49 +0100

    feat: add accordion for riddle in admin

--- chasse-au-tresor-frontend/src/app/admin/dashboard/riddle/riddle.component.ts
+++ chasse-au-tresor-frontend/src/app/admin/dashboard/riddle/riddle.component.ts
@@ -3,15 +3,37 @@ import { Component, Input } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { TeamRiddle } from '../../../reference/types';
 import { SolutionsComponent } from '../solutions/solutions.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-riddle',
   standalone: true,
-  imports: [CommonModule, MatDividerModule, SolutionsComponent],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatExpansionModule,
+    SolutionsComponent,
+  ],
   templateUrl: './riddle.component.html',
-  styleUrl: './riddle.component.scss'
+  styleUrl: './riddle.component.scss',
 })
 export class RiddleComponent {
-
   @Input() teamRiddle: TeamRiddle | undefined;
+
+  actionNeeded() {
+    console.log('actionNeeded');
+    if (!this.teamRiddle) {
+      return false;
+    }
+
+    let ret = false;
+    this.teamRiddle.solutions.forEach((sol) => {
+      if (sol.validated !== true && sol.validated !== false) {
+        ret = true;
+      }
+    });
+    return ret;
+  }
 }
