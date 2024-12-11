80baa56b1082aa85294037eab0ae843190f6fd8c
bibulle <famille.martin@gmail.com>
2024-12-11 17:53:57 +0100

    fix: remove old token if needed

--- chasse-au-tresor-frontend/src/app/core/auth.guard.ts
+++ chasse-au-tresor-frontend/src/app/core/auth.guard.ts
@@ -13,19 +13,26 @@ export class AuthGuard implements CanActivate {
     const token = localStorage.getItem('token');
 
     if (token) {
-      const decrypted = this.authService.decryptToken(token);
-      if (decrypted && decrypted.username) {
-        const username = decrypted.username.charAt(0).toUpperCase() + decrypted.username.slice(1);
-        localStorage.setItem('createdUser', `{"username": "${username}"}`);
+      try {
+        const decrypted = this.authService.decryptToken(token);
+        if (decrypted && decrypted.username) {
+          const username = decrypted.username.charAt(0).toUpperCase() + decrypted.username.slice(1);
+          localStorage.setItem('createdUser', `{"username": "${username}"}`);
 
-        this.playerService.alreadyExists(username).subscribe((alreadyExists) => {
-          if (!alreadyExists) {
-            this.playerService.createPlayer({ username: username }).subscribe();
-          }
-        });
+          this.playerService.alreadyExists(username).subscribe((alreadyExists) => {
+            if (!alreadyExists) {
+              this.playerService.createPlayer({ username: username }).subscribe();
+            }
+          });
+        }
+
+        return true; // L'utilisateur est authentifié
+      } catch (error) {
+        console.error('Erreur lors du traitement du token:', error);
+        localStorage.removeItem('token');
+        this.router.navigate(['/login']); // Redirige vers la page de connexion
+        return false;
       }
-
-      return true; // L'utilisateur est authentifié
     } else {
       this.router.navigate(['/login']); // Redirige vers la page de login
       return false;
